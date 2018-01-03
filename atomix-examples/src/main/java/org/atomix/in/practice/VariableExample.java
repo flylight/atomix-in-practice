0d5cab874910db4308e9bc6466779aa50354184e
Andriy Rymar <andriyrav@gmail.com>
2018-01-03 15:27:30 +0200

    Variable is done (except close/open functionality)

--- atomix-examples/src/main/java/org/atomix/in/practice/VariableExample.java
+++ atomix-examples/src/main/java/org/atomix/in/practice/VariableExample.java
@@ -8,27 +8,96 @@ import io.atomix.catalyst.concurrent.Listener;
 import io.atomix.variables.AbstractDistributedValue;
 import io.atomix.variables.AbstractDistributedValue.ChangeEvent;
 
+/**
+ * This class demonstrates how to use Distributed Variable features.
+ *
+ * NOTICE : Not all possible methods are covered (for example Get with Consistency Level ot operations with TTL).
+ */
 public class VariableExample {
 
+  /**
+   * Set Value to {@link io.atomix.variables.DistributedValue} by Key and return {@link CompletableFuture} wth no result.
+   *
+   * @param replica Atomix cluster replica.
+   * @param key Variable key.
+   * @param value new Value.
+   * @param <T> Type of variable value.
+   * @return {@link CompletableFuture} with no result.
+   */
   public <T> CompletableFuture<Void> setValue(AtomixReplica replica, String key, T value) {
 
     return replica.<T>getValue(key).thenCompose(dv -> dv.set(value));
   }
 
+  /**
+   * Get {@link io.atomix.variables.DistributedValue} by  Key and then get actual value as {@link CompletableFuture}.
+   *
+   * @param replica Atomix cluster replica.
+   * @param key Variable key.
+   * @param <T> Type of variable value.
+   * @return {@link CompletableFuture} with actual value result.
+   */
   public <T> CompletableFuture<T> readValue(AtomixReplica replica, String key) {
 
     return replica.<T>getValue(key).thenCompose(AbstractDistributedValue::get);
   }
 
+  /**
+   * Delete variable by Key.
+   *
+   * @param replica Atomix cluster replica.
+   * @param key Variable key.
+   * @return {@link CompletableFuture} with no result.
+   */
   public CompletableFuture<Void> deleteValue(AtomixReplica replica, String key) {
 
     return replica.getValue(key).thenCompose(AbstractDistributedValue::delete);
   }
 
+  /**
+   * Add listener to variable to listen events related to this variable.
+   *
+   * @param replica Atomix cluster replica.
+   * @param key Variable key.
+   * @param listener Listener.
+   * @param <T> Type of variable value.
+   * @return {@link CompletableFuture} that contains same listener.
+   */
   public <T> CompletableFuture<Listener<ChangeEvent<T>>> listenValueChanges(AtomixReplica replica, String key,
                                                                             Consumer<ChangeEvent<T>> listener) {
 
     return replica.<T>getValue(key).thenCompose(distributedValue -> distributedValue.onChange(listener));
   }
+
+  /**
+   * Set value only if expected value equals actual.
+   *
+   * @param replica Atomix cluster replica.
+   * @param key Variable key.
+   * @param expected Expected value that should match actual.
+   * @param value New value of variable that should be set if compare operation will be successful.
+   * @param <T> Type of variable value.
+   * @return {@link CompletableFuture} with result of operation.
+   */
+  public <T> CompletableFuture<Boolean> compareAndSetValue(AtomixReplica replica, String key, T expected, T value) {
+
+    return replica.<T>getValue(key).thenCompose(distributedValue -> distributedValue.compareAndSet(expected, value));
+  }
+
+  //  TODO need time to understand
+//  public <T> CompletableFuture<DistributedValue<T>> changeValueStateToOpen(AtomixReplica replica, String key) {
+//
+//    return replica.<T>getValue(key).thenCompose(AbstractResource::open);
+//  }
+//
+//  public CompletableFuture<Void> changeValueStateToClose(AtomixReplica replica, String key) {
+//
+//    return replica.getValue(key).thenCompose(AbstractResource::close);
+//  }
+//
+//  public <T> CompletableFuture<DistributedValue<T>> getDistributedValue(AtomixReplica replica, String key) {
+//
+//    return replica.getValue(key);
+//  }
 
 }
--- atomix-examples/src/test/java/org/atomix/in/practice/VariableExampleTest.java
+++ atomix-examples/src/test/java/org/atomix/in/practice/VariableExampleTest.java
@@ -12,13 +12,14 @@ import io.atomix.AtomixReplica;
 import io.atomix.variables.AbstractDistributedValue.ChangeEvent;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class VariableExampleTest {
 
+  private static final String TEST_KEY = "test-key";
   private static final ClusterExample CLUSTER_EXAMPLE = new ClusterExample();
   private static final VariableExample VARIABLE_EXAMPLE = new VariableExample();
-
   private static AtomixReplica[] cluster;
 
   @BeforeClass
@@ -34,13 +35,11 @@ public class VariableExampleTest {
   @Test
   public void setAndCheckVariableCorrectlyTest() {
 
-    String variableKey = "test-key";
-
     String variableMessage = "Hello world!";
 
-    Boolean resultOfComparison = VARIABLE_EXAMPLE.setValue(cluster[0], variableKey, variableMessage)
+    Boolean resultOfComparison = VARIABLE_EXAMPLE.setValue(cluster[0], TEST_KEY, variableMessage)
         .thenCompose(aVoid -> CompletableFuture.completedFuture(cluster[1]))
-        .thenCompose(atomixReplica -> VARIABLE_EXAMPLE.readValue(atomixReplica, variableKey))
+        .thenCompose(atomixReplica -> VARIABLE_EXAMPLE.readValue(atomixReplica, TEST_KEY))
         .thenCompose(value -> CompletableFuture.completedFuture(variableMessage.equals(value)))
         .join();
 
@@ -50,12 +49,11 @@ public class VariableExampleTest {
   @Test
   public void setAndCheckVariableIncorrectlyTest() {
 
-    String variableKey = "test-key";
     String variableKey2 = "test-key2";
 
     String variableMessage = "Hello world!";
 
-    Boolean resultOfComparison = VARIABLE_EXAMPLE.setValue(cluster[0], variableKey, variableMessage)
+    Boolean resultOfComparison = VARIABLE_EXAMPLE.setValue(cluster[0], TEST_KEY, variableMessage)
         .thenCompose(aVoid -> CompletableFuture.completedFuture(cluster[1]))
         .thenCompose(atomixReplica -> VARIABLE_EXAMPLE.readValue(atomixReplica, variableKey2))
         .thenCompose(value -> CompletableFuture.completedFuture(Objects.isNull(value)))
@@ -67,16 +65,14 @@ public class VariableExampleTest {
   @Test
   public void setAndDeleteVariableTest() {
 
-    String variableKey = "test-key";
-
     String variableMessage = "Hello world!";
 
     Boolean resultOfComparison =
-        VARIABLE_EXAMPLE.setValue(cluster[0], variableKey, variableMessage)
+        VARIABLE_EXAMPLE.setValue(cluster[0], TEST_KEY, variableMessage)
             .thenCompose(aVoid -> CompletableFuture.completedFuture(cluster[1]))
-            .thenCompose(atomixReplica -> VARIABLE_EXAMPLE.deleteValue(atomixReplica, variableKey)
+            .thenCompose(atomixReplica -> VARIABLE_EXAMPLE.deleteValue(atomixReplica, TEST_KEY)
                 .thenCompose(aVoid -> CompletableFuture.completedFuture(atomixReplica)))
-            .thenCompose(atomixReplica -> VARIABLE_EXAMPLE.readValue(atomixReplica, variableKey))
+            .thenCompose(atomixReplica -> VARIABLE_EXAMPLE.readValue(atomixReplica, TEST_KEY))
             .thenCompose(value -> CompletableFuture.completedFuture(Objects.isNull(value)))
             .join();
 
@@ -85,7 +81,6 @@ public class VariableExampleTest {
 
   @Test
   public void listenEventChanges() throws InterruptedException {
-    String valueKey = "test-key";
 
     Integer oldValue = 5;
     Integer newValue = 10;
@@ -104,11 +99,11 @@ public class VariableExampleTest {
 
     };
 
-    VARIABLE_EXAMPLE.setValue(cluster[0], valueKey, oldValue)
+    VARIABLE_EXAMPLE.setValue(cluster[0], TEST_KEY, oldValue)
         .thenCompose(aVoid -> CompletableFuture.completedFuture(cluster))
-        .thenCompose(atomixReplicas -> VARIABLE_EXAMPLE.listenValueChanges(atomixReplicas[1], valueKey, listener)
+        .thenCompose(atomixReplicas -> VARIABLE_EXAMPLE.listenValueChanges(atomixReplicas[1], TEST_KEY, listener)
             .thenCompose(fn -> CompletableFuture.completedFuture(atomixReplicas[0])))
-        .thenCompose(atomixReplica -> VARIABLE_EXAMPLE.setValue(atomixReplica, valueKey, newValue)).join();
+        .thenCompose(atomixReplica -> VARIABLE_EXAMPLE.setValue(atomixReplica, TEST_KEY, newValue)).join();
 
     //Test going to wait until listener will be invoked and notify execution
     synchronized (this) {
@@ -118,4 +113,47 @@ public class VariableExampleTest {
     assertEquals(true, results[0]);
     assertEquals(true, results[1]);
   }
+
+  @Test
+  public void compareAndSetVariableTest() {
+
+    int value1 = 1;
+    int value2 = 2;
+    int value3 = 3;
+
+    Boolean successfulResult = VARIABLE_EXAMPLE
+        .setValue(cluster[0], TEST_KEY, value1).thenCompose(aVoid -> VARIABLE_EXAMPLE
+            .compareAndSetValue(cluster[1], TEST_KEY, value1, value2)).join();
+
+    Boolean unsuccessfulResult = VARIABLE_EXAMPLE
+        .compareAndSetValue(cluster[1], TEST_KEY, value1, value3).join();
+
+    assertTrue(successfulResult);
+    assertFalse(unsuccessfulResult);
+  }
+
+
+  //  TODO doesn't work as expected
+//   @Test
+//  public void openAndCloseVariableStateTest() throws InterruptedException {
+//
+//    String initialValue = "initial";
+//    String value = "test-value";
+//
+//    Boolean isClosed = VARIABLE_EXAMPLE.setValue(cluster[0], TEST_KEY, initialValue)
+//        .thenCompose(aVoid -> VARIABLE_EXAMPLE.changeValueStateToClose(cluster[1], TEST_KEY))
+//        .thenCompose(aVoid -> VARIABLE_EXAMPLE.getDistributedValue(cluster[1], TEST_KEY))
+//        .thenCompose(distributedValue -> CompletableFuture.completedFuture(distributedValue.isClosed()))
+//        .join();
+//
+//    Thread.sleep(5000);
+//
+//    Boolean isClosed2 = VARIABLE_EXAMPLE.getDistributedValue(cluster[1], TEST_KEY)
+//        .thenCompose(distributedValue -> CompletableFuture.completedFuture(distributedValue.isClosed()))
+//        .join();
+//
+//    assertFalse(isClosed);
+//    assertTrue(isClosed2);
+//
+//  }
 }
